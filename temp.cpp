28fcbc5a8734633c16f69a68e34acb6d1a2e5e4e
Tahmid Mahmud <73707938+Tahmid-Mahmud@users.noreply.github.com>
2021-09-13 11:40:30 +0600

    B

--- temp.cpp
+++ temp.cpp
@@ -32,57 +32,6 @@ int main(){
 	}
 }
 /*
-//         Palindrome          //
-#include<iostream>
-#include<algorithm>
 
-using namespace std;
-
-int main(){
-	int t;
-	cin>>t;
-	
-	for(int i=1; i<=t; i++){
-    	string palindrom, repl;
-    	cin>>palindrom;
-    	repl=palindrom;
-    	
-    	reverse(palindrom.begin(), palindrom.end());
-    	if (repl==palindrom){
-			printf("Case %d: Yes\n", i);
-		}else{
-			printf("Case %d: No\n", i);
-		}
-	}
-	
-	return 0;
-}
 //         Higher  Math        //
-#include<iostream>
-#include<algorithm>
-#include<vector>
-using namespace std;
-
-int main(){
-    int t;
-    vector<int> sides;
-    cin>>t;
-    
-    for(int i=1; i<=t; i++){
-    	int a, b, c;
-    	cin>>a>>b>>c;
-    	
-    	sides.push_back(a);
-		sides.push_back(b);
-		sides.push_back(c);
-		sort(sides.begin(), sides.end());
-		
-		int rhs=sides[0]*sides[0]+sides[1]*sides[1];
-		if ((sides[2]*sides[2])==rhs){
-			printf("Case %d: yes\n", i);
-		}else{
-			printf("Case %d: no\n", i);
-		}
-		sides.clear();
-    }
-}     */
+     */
