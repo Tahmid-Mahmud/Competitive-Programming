e5941686e7d1607f0b33384e4a896f6e7d8a5e70
Tahmid Mahmud <73707938+Tahmid-Mahmud@users.noreply.github.com>
2021-09-15 11:48:31 +0600

    Add files via upload

---
+++ LightOJ/array.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	for(int i=1; i<=t; i++){
+		int n, m;
+		cin>>n>>m;
+		vector<int> a;
+		for(int j=0; j<n; j++){
+			int temp;
+			cin>>temp;
+			a.push_back(temp);
+		}
+		
+		for(int k=0; k<=m; k++){
+			string oper;
+			//cin.ignore();
+			getline(cin, oper);
+//			cout<<"Turn no: "<<k<<endl;
+//			cout<<"CMD: "<<oper<<endl;
+			if(oper[0]=='S')
+			    for(int l=0; l<a.size(); l++){
+			    	a[l]+=oper[2]-'0';
+			    }
+			
+			if(oper[0]=='M')
+			    for(int l=0; l<a.size(); l++){
+			    	a[l]*=oper[2]-'0';
+			    }
+			    
+			if(oper[0]=='D')
+			    for(int l=0; l<a.size(); l++){
+			    	a[l]/=oper[2]-'0';
+			    }
+			    
+			if(oper[0]=='P')
+			    swap(a[oper[2]-'0'], a[oper[4]-'0']);
+			    
+			if(oper[0]=='R')
+			    reverse(a.begin(), a.end());
+			
+		}
+		printf("Case %d: ", i);
+		for(int q:a){
+			cout<<q<<" ";
+		}cout<<endl;
+	}
+	return 0;
+}
